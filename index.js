a109e485d9fdd813d835d1b51efb3691fa67397e
mahalepranav <pranavmahale735@gmail.com>
2025-02-20 13:28:45 +0530

    first patch

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-    origin: process.env.FRONTEND_URL,  
+    origin: "https://stationary-ecommerce-frontend.vercel.app",  
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,  
